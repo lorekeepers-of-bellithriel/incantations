03c5d2a6a9fea02bb7dfe886b0e24174fc1e4f59
obdsf <obdsf0@gmail.com>
2023-01-04 04:14:21 +0200

    working on configuration

--- cna/src/main.ts
+++ cna/src/main.ts
@@ -1,58 +1,91 @@
 import { Scribe, LogLevels } from "@lorekeepers-of-bellithriel/scribe";
+import { MergeExclusive, RequireAtLeastOne } from "type-fest";
+import lodashMerge from "lodash.merge";
 import is from "@sindresorhus/is";
+import fs from "node:fs/promises";
 import esbuild from "esbuild";
 import path from "node:path";
-import fs from "node:fs/promises";
 
 // todo: move these types/enums/consts/functions into a separate package that's used by all cli-like apps
 //#region temp config types
 const LOB_CONFIG_FILE_ARG_NAME = "--config=";
 
-const STANDARD_CONFIGURATION_FILES = [
-    // programmatic config file
-    "lob.config.js",
-    // json config file
-    "lob.json",
-] as const;
-type ConfigurationFiles = typeof STANDARD_CONFIGURATION_FILES[number];
-const isConfigurationFiles = (value: unknown): value is ConfigurationFiles => {
-    return STANDARD_CONFIGURATION_FILES.includes(value as ConfigurationFiles);
+enum StandardConfigurationFiles {
+    Programmatic = "lob.config.js",
+    Json = "lob.json",
+}
+export const isStandardConfigurationFiles = (value: unknown): value is StandardConfigurationFiles => {
+    return Object.values(StandardConfigurationFiles).includes(value as StandardConfigurationFiles);
 };
 
-type ConfigurationFile = ConfigurationFiles | string;
-const isConfigurationFile = (value: unknown): value is ConfigurationFile => {
-    const val = value as ConfigurationFile;
-    return isConfigurationFiles(val) || is.string(val);
-};
+type StandardConfigurationFile = { standard: StandardConfigurationFiles };
+type CustomConfigurationFile = { custom: string };
+type ConfigurationFile = MergeExclusive<StandardConfigurationFile, CustomConfigurationFile>;
 
 type LobConfiguration = {
     incantations: Incantations;
 };
-const isLobConfiguration = (value: unknown): value is LobConfiguration => {
-    const val = value as LobConfiguration;
+const defaultLobConfiguration = (): LobConfiguration => {
+    return {
+        incantations: {
+            cna: {
+                rootDir: ".",
+                kappa: "keepo",
+            },
+        },
+    };
+};
+type RawLobConfiguration = RequireAtLeastOne<{
+    incantations: RawIncantations;
+}>;
+const isRawLobConfiguration = (value: unknown): value is RawLobConfiguration => {
+    const val = value as RawLobConfiguration;
     if (!is.nonEmptyObject(val)) return false;
-    else if (!isIncantations(val.incantations)) return false;
-    else return true;
+    let count = 0;
+    if (!is.undefined(val.incantations)) {
+        if (!isRawIncantations(val.incantations)) return false;
+        count++;
+    }
+    return count !== 0;
 };
 
 type Incantations = {
     cna: CnaIncantation;
 };
-const isIncantations = (value: unknown): value is Incantations => {
-    const val = value as Incantations;
+type RawIncantations = RequireAtLeastOne<{
+    cna: RawCnaIncantation;
+}>;
+const isRawIncantations = (value: unknown): value is RawIncantations => {
+    const val = value as RawIncantations;
     if (!is.nonEmptyObject(val)) return false;
-    else if (!isCnaIncantation(val.cna)) return false;
-    else return true;
+    let count = 0;
+    if (!is.undefined(val.cna)) {
+        if (!isRawCnaIncantation(val.cna)) return false;
+        count++;
+    }
+    return count !== 0;
 };
 
 type CnaIncantation = {
     rootDir: string;
+    // todo: remove
+    kappa: "keepo";
 };
-const isCnaIncantation = (value: unknown): value is CnaIncantation => {
-    const val = value as CnaIncantation;
+type RawCnaIncantation = RequireAtLeastOne<CnaIncantation>;
+const isRawCnaIncantation = (value: unknown): value is RawCnaIncantation => {
+    const val = value as RawCnaIncantation;
     if (!is.nonEmptyObject(val)) return false;
-    else if (!is.string(val.rootDir)) return false;
-    else return true;
+    let count = 0;
+    if (!is.undefined(val.rootDir)) {
+        if (!is.string(val.rootDir)) return false;
+        count++;
+    }
+    return count !== 0;
+};
+
+const mergeRawLobConfiguration = (raw: RawLobConfiguration): LobConfiguration => {
+    const def: LobConfiguration = defaultLobConfiguration();
+    return lodashMerge(def, raw);
 };
 
 const fileExists = async (file: string): Promise<boolean> => {
@@ -68,16 +101,41 @@ const fileExists = async (file: string): Promise<boolean> => {
 
 const determineConfigurationFile = async (args: string[]): Promise<ConfigurationFile | null> => {
     const arg = args.find((arg) => arg.startsWith(LOB_CONFIG_FILE_ARG_NAME));
-    if (!is.undefined(arg)) return arg.replace(LOB_CONFIG_FILE_ARG_NAME, "");
-    for (const standardConfigFile of STANDARD_CONFIGURATION_FILES) {
-        if (await fileExists(standardConfigFile)) return standardConfigFile;
+    if (!is.undefined(arg)) return { custom: arg.replace(LOB_CONFIG_FILE_ARG_NAME, "") };
+    for (const standardConfigFile of Object.values(StandardConfigurationFiles)) {
+        if (await fileExists(standardConfigFile)) return { standard: standardConfigFile };
     }
     return null;
 };
 
-const determineConfiguration = async (args: string[]): LobConfiguration => {
+const determineConfiguration = async (args: string[]): Promise<LobConfiguration | null> => {
     const configFile = await determineConfigurationFile(args);
-    // todo: implement
+    // no config file available, use default configuration
+    if (is.null_(configFile)) return defaultLobConfiguration();
+    const prefix = "could not determine configuration";
+    const file = path.resolve(configFile.custom === undefined ? configFile.standard : configFile.custom);
+    if (file.endsWith(".js")) {
+        scribe.error(prefix, "programmatic configuration file is not supported yet, try using a json configuration file instead");
+        return null;
+    } else if (file.endsWith(".json")) {
+        let raw: unknown;
+        try {
+            const buf = await fs.readFile(file);
+            raw = JSON.parse(buf.toString());
+        } catch (err) {
+            scribe.error(prefix, err);
+            return null;
+        }
+        if (!isRawLobConfiguration(raw)) {
+            scribe.error(prefix, "incorrect json configuration file structure in", file);
+            return null;
+        }
+        const def = defaultLobConfiguration();
+        return lodashMerge(def, raw);
+    } else {
+        scribe.error(prefix, "unknown configuration file extension in", file);
+        return null;
+    }
 };
 //#endregion
 
@@ -118,6 +176,7 @@ const setup = async () => {
     } catch (err) {
         scribe.error("error during setup", err);
     }
+    // todo: force type module in package.json
 };
 
 const dev = async () => {
